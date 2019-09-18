f6b798d845c97dad2c20b31d5fb12fca15066f06
Jesse Suomalainen <je-suomalaine@student.saimia.fi>
2019-09-18 15:37:54 +0300

    added comments

--- factorial/factorial/factorial/Program.cs
+++ factorial/factorial/factorial/Program.cs
@@ -5,26 +5,34 @@ namespace kertoma
     class Program
     { static void Main(string[] args)
         {
+                //define variables
             int n = 0;
             int fact = 1;
 
+
+                //print message
             Console.WriteLine("Kertoman laskenta");
             Console.Write("Syötä minkä luvun kertoma lasketaan? ");
+                
+                //force integer
             n = int.Parse(Console.ReadLine());
             
+
+                //logic
             if(n < 0)
             {
                 Console.WriteLine("Virheellinen syöte");
             }
             else
             {
+                //loop for defining fact
                 while(n > 0)
                 {
                     fact = fact * n;
                     n = n - 1;
                 }
+                //print answer
                 Console.WriteLine("Vastaus: " + fact);
-                //huomio
             }
         }
     }
