9dcd8056c1a7ddfb25a6f90343ea76b58b791ffc
Jesse Suomalainen <je-suomalaine@student.saimia.fi>
2019-09-18 15:25:30 +0300

    added kertoma

--- factorial/factorial/factorial/Program.cs
+++ factorial/factorial/factorial/Program.cs
@@ -24,6 +24,7 @@ namespace kertoma
                     n = n - 1;
                 }
                 Console.WriteLine("Vastaus: " + fact);
+                //huomio
             }
         }
     }
