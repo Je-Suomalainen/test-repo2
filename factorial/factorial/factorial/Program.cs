4085116312be6bcc0d80aa6f1e13a3f4281afb7d
Jesse Suomalainen <je-suomalaine@student.saimia.fi>
2019-09-18 15:23:26 +0300

    added code

--- factorial/factorial/factorial/Program.cs
+++ factorial/factorial/factorial/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace factorial
+namespace kertoma
 {
     class Program
-    {
-        static void Main(string[] args)
+    { static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 0;
+            int fact = 1;
+
+            Console.WriteLine("Kertoman laskenta");
+            Console.Write("Syötä minkä luvun kertoma lasketaan? ");
+            n = int.Parse(Console.ReadLine());
+            
+            if(n < 0)
+            {
+                Console.WriteLine("Virheellinen syöte");
+            }
+            else
+            {
+                while(n > 0)
+                {
+                    fact = fact * n;
+                    n = n - 1;
+                }
+                Console.WriteLine("Vastaus: " + fact);
+            }
         }
     }
 }
