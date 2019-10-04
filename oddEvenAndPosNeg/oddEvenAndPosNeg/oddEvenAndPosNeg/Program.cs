96e51d076715e978529bc21ee4fa5979d03ec275
Jesse Suomalainen <je-suomalaine@student.saimia.fi>
2019-10-04 03:20:59 +0300

    new code added

---
+++ oddEvenAndPosNeg/oddEvenAndPosNeg/oddEvenAndPosNeg/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace oddEvenAndPosNeg
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("syötä luku");
+            int num = int.Parse(Console.ReadLine());
+            string pos;
+            string par;
+
+            if (num %2 == 0)
+            {
+                par = "parillinen";
+            }
+            else
+            {
+                par = "pariton";
+            }
+            if (num == 0)
+            {
+                pos = "nolla";
+            }
+            {
+                Console.WriteLine(par);
+            }
+        }
+    }
+}
