9c5b3013554dc87d3be403ebd2660c827f0a9589
Jesse Suomalainen <je-suomalaine@student.saimia.fi>
2019-10-30 10:31:56 +0200

    new project

---
+++ NPariPariton/NPariPariton/NPariPariton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NPariPariton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N:n parittomien ja parillisten lukujen summan");
+            Console.WriteLine("Syötä luku: ");
+            int i = 1;
+            int pariton = 0;
+            int parill = 0;
+            int userNum = 0;
+        }   
+    }
+}
