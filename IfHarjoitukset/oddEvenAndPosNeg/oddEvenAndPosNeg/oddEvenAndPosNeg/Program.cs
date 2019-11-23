580a6a76705fc0c8c850208059213cc9c2bd7014
Je-Suomalainen <54808528+Je-Suomalainen@users.noreply.github.com>
2019-11-23 12:00:22 +0200

    new folder for better order

---
+++ IfHarjoitukset/oddEvenAndPosNeg/oddEvenAndPosNeg/oddEvenAndPosNeg/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace oddEvenAndPosNeg
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Ohjelma tarkastaa onko syöttämäsi luku parillinen vai pariton" + 
+                " sekä onko se negatiivinen, positiivinen vai nollaluku");
+            Console.WriteLine(" ");                                                                 //added empty line
+            Console.WriteLine("Syötä kokonaisluku: ");                                              //defined integer
+            string pos = "positiivinen";                                                        
+            string par = "parillinen";                                                              //variables
+            int num = int.Parse(Console.ReadLine());                                                //string to 32-bit integer
+
+            {
+                if (num == 0) ;                                                                     //pos or neg. Could also use if else?
+            }
+            {
+                pos = "nolla";
+            }
+            if (num < 0)
+            {
+                pos = "Negatiivinen";
+            }
+            if (num > 0)
+            {
+                pos = "Positiivinen";
+            }       
+
+            if (num % 2 == 0)                                                                       //modulo
+            {
+                par = "Parillinen";
+            }
+            else
+            {
+                par = "Pariton";
+            }
+
+            Console.WriteLine($"Numero {num} on {pos} ja se on {par}");                             //printing using variables
+
+        }
+    }
+}
---
+++ IfHarjoitukset/oddOrEven/oddOrEven/oddOrEven/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace posOrNeg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("syötä luku: ");
+            num = Int32.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("luku on parillinen");
+            }
+            else
+            {
+                Console.WriteLine("luku on pariton");
+            }
+        }
+    }
+}
