cb565852d63e6d82a23ba6c12f6c45ad7671fd99
Je-Suomalainen <54808528+Je-Suomalainen@users.noreply.github.com>
2019-11-23 12:27:16 +0200

    new folder for better order

---
+++ DoWhileFrorHarjoitukset/NPariPariton/NPariPariton/NPariPariton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NPariPariton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N:n parittomien ja parillisten lukujen summan");
+            Console.WriteLine("Syötä luku: ");
+            
+            int oddS = 0;
+            int evenS = 0;
+            int userNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= userNum; ++i)
+            {
+                if (i % 2 == 0)
+                {
+                    evenS = evenS + i;
+                }
+                else
+                {
+                    oddS = oddS + i;
+                }
+            }
+            Console.WriteLine($"Parillisten summa = {evenS}\n" + $"Parittomien summa = {oddS}");
+        }
+    }
+}
---
+++ DoWhileFrorHarjoitukset/nKertoma/nKertoma/nKertoma/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace nKertoma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee syöttämäsi luvun kertoman");
+            Console.WriteLine("Syötä luku: ");
+
+            int userN = int.Parse(Console.ReadLine());
+            int F = 1;
+            int i = 1;
+
+            if (userN < 0)
+            {
+                Console.WriteLine("Syöttämäsi luku ei ole positiivinen! ");
+            }
+            else
+            {
+                while (i <= userN)
+
+                {
+                    F = F * i;
+                    i = i + 1;
+                }
+                {
+                    Console.WriteLine("Lukusi kertoma on " + F);
+                }
+            }
+
+        }
+    }
+}
